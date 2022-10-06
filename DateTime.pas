9918b843f2492861ac481d1823d0c2bbea38d15f
Ксения Николаенко <ksenya.nikolaenko.67@gmail.com>
2022-10-06 11:30:02 +0300

    Revert "Високосный год"
    This reverts commit 2262945205bd5eed0bd88af580288ec55587fbc1.

--- DateTime.pas
+++ DateTime.pas
@@ -1,8 +1,6 @@
 ﻿begin
-  var (year,min):=ReadInteger2('Ввод');
+  var year:=ReadInteger('Ввод');
   var flag:=false;
-  var sec:integer;
   if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then flag:=true;
-  sec:=min * 60;
-  print('Вывод',flag, sec);
+  print('Вывод',flag);
 end.
