169dd1f33b782c1934fbb3f549391df65619d6c4
Ксения Николаенко <ksenya.nikolaenko.67@gmail.com>
2022-10-06 11:36:02 +0300

    Високосный год

--- DateTime.pas
+++ DateTime.pas
@@ -1,6 +1,10 @@
 ﻿begin
-  var year:=ReadInteger('Ввод');
+  var (year,min,day1,day2):=ReadInteger4('Ввод');
   var flag:=false;
+  var sec:integer;
   if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then flag:=true;
-  print('Вывод',flag);
+  sec:=min * 60;
+  if (365 - day1 < 365 - day2) then print(day1)
+  else print('Вывод',day2);
+  print(flag, sec);
 end.
